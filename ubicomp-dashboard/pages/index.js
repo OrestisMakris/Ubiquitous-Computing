4456e464916b57075af395000d86b454cacec516
OrestisMakris <79021400+OrestisMakris@users.noreply.github.com>
2025-06-14 18:49:14 +0300

    fixed index

--- ubicomp-dashboard/pages/index.js
+++ ubicomp-dashboard/pages/index.js
@@ -1,5 +1,17 @@
-import AllDashboards from '../components/AllDashboards';
+import Head from 'next/head'
+import AllDashboards from '@/components/AllDashboards'
 
 export default function Home() {
-  return <AllDashboards />;
+  return (
+    <>
+      <Head>
+        <title>UbiComp Dashboards</title>
+      </Head>
+      <main className="container mx-auto p-6">
+        <h1 className="text-4xl font-bold mb-8">UbiComp Monitoring</h1>
+        {/* this is where your tabbed dashboards live */}
+        <AllDashboards />
+      </main>
+    </>
+  )
 }
