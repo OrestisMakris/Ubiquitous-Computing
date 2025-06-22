4089360e13e464ef2954fe5d8aee423388682639
OrestisMakris <79021400+OrestisMakris@users.noreply.github.com>
2025-06-22 18:37:19 +0300

    Decrease header font size and increase daily count font size for improved readability in DashboardOne

--- ubicomp-dashboard/components/DashboardOne.js
+++ ubicomp-dashboard/components/DashboardOne.js
@@ -37,7 +37,7 @@ export default function DashboardOne() {
   return (
     <div className="space-y-10">
       <header className="text-center py-6">
-        <p className="text-5xl text-[#0017a5] font-extrabold">
+        <p className="text-4xl text-[#0017a5] font-extrabold">
           📡 UbiComp Live Presence Dashboard
         </p>
         <p className="mt-28 text-sm text-gray-600">
@@ -61,7 +61,7 @@ export default function DashboardOne() {
             <CardTitle> 🔹Μοναδικοί Επισκέπτες Σήμερα</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-4xl text-[#0017a5] font-extrabold">{dailyCount}</p>
+            <p className="text-8xl text-[#0017a5] font-extrabold">{dailyCount}</p>
           </CardContent>
         </Card>
 
