f49f56cc989d1a9316432c2d177b2fde815b59f8
OrestisMakris <79021400+OrestisMakris@users.noreply.github.com>
2025-06-22 19:26:18 +0300

    Update header and footer styles in DashboardOne for improved readability and consistency

--- ubicomp-dashboard/components/DashboardOne.js
+++ ubicomp-dashboard/components/DashboardOne.js
@@ -37,7 +37,7 @@ export default function DashboardOne() {
   return (
     <div className="space-y-10">
       <header className="text-center py-6">
-        <p className="text-6xl text-[#0017a5] font-semibold">
+        <p className="text-4xl text-[#0017a5]-500 font-extrabold">
           📡 Live Presence Dashboard 1
         </p>
       </header>
@@ -121,7 +121,7 @@ export default function DashboardOne() {
     </p>
   </CardContent>
 </Card>
-      <footer className="text-center text-base text-gray-500 font-semibold mt-10">
+      <footer className="text-center text-base text-gray-700 font-bold mt-10">
         © 2025 | CEID_NE576 — Ubiquitous Computing Lab Exercise  2024/25<br/>
         👤 Orestis Antonis Makris (AM 1084516) Prof. Andreas Komninos
       </footer>
