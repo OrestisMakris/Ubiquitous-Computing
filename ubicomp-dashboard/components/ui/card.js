156c21886df3dbac2c5c598f746e675013377c00
OrestisMakris <79021400+OrestisMakris@users.noreply.github.com>
2025-06-22 19:11:42 +0300

    Remove color property from CardContent for improved consistency

--- ubicomp-dashboard/components/ui/card.js
+++ ubicomp-dashboard/components/ui/card.js
@@ -13,5 +13,5 @@ export function CardTitle({ children, ...props }) {
 }
 
 export function CardContent({ children, ...props }) {
-  return <div style={{ fontWeight: 600 , marginLeft: 40 , fontSize: '2rem' ,color: '#0f1d7a'}} {...props}>{children}</div>;
+  return <div style={{ fontWeight: 600 , marginLeft: 40 , fontSize: '2rem' }} {...props}>{children}</div>;
 }
