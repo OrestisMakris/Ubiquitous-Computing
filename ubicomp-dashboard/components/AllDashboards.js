003e0cf17d54a4e5bb0e215b2b6c28440d633000
OrestisMakris <79021400+OrestisMakris@users.noreply.github.com>
2025-06-23 16:29:47 +0300

    Refactor AllDashboards: adjust logo position to the left for improved layout

--- ubicomp-dashboard/components/AllDashboards.js
+++ ubicomp-dashboard/components/AllDashboards.js
@@ -23,7 +23,7 @@ export default function AllDashboards() {
         alt="Logo"
         width={32}
         height={32}
-        className="absolute top-4 left-4"
+        className="absolute top-4 left-400"
       />
 
       <Tabs value={tab} onValueChange={setTab}>
